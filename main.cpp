78ec594a97f9a351b43935ebcef057d32d3db244
Maxime ANDRE <maxime.andre@gadz.org>
2019-07-30 21:33:23 +0200

    Gestion des exceptions dans main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <exception>
+#include <QDebug>
 
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
+    try {
+        QApplication a(argc, argv);
+        MainWindow w;
 
-    w.show();
-    //w.showFullScreen();
-
-    return a.exec();
+        w.show();
+        //w.showFullScreen();
+        return a.exec();
+    } catch (QString qs) {
+        qDebug() << "Caught exception of type QString : " << qs;
+        return EXIT_FAILURE;
+    } catch (std::exception e) {
+        qDebug() << "Caught exception of type exception : " << e.what();
+        return EXIT_FAILURE;
+    }
 }
