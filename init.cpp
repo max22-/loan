0374d0c1e8e2479f24b423eee1f409e69315a817
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-08 16:09:44 +0200

    Ajout de l'affichage des info du périphérique de capture dans le log

--- init.cpp
+++ init.cpp
@@ -2,6 +2,22 @@
 #include <QDebug>
 #include "config.h"
 #include <QProcess>
+#include <QAudioDeviceInfo>
+
+void showAudioDeviceInfo() {
+    QAudioDeviceInfo info = QAudioDeviceInfo::defaultInputDevice();
+    qDebug() << "******** Audio device info ********";
+    qDebug() << "- Device name : " << info.deviceName();
+    qDebug() << "Available audio formats :";
+    qDebug() << "- Preferred format : " << info.preferredFormat();
+    qDebug() << "- Supported byte orders : " << info.supportedByteOrders();
+    qDebug() << "- Supported channel counts : " << info.supportedChannelCounts();
+    qDebug() << "- Supported codecs : " << info.supportedCodecs();
+    qDebug() << "- Supported sample rates : " << info.supportedSampleRates();
+    qDebug() << "- Supported sample sizes : " << info.supportedSampleSizes();
+    qDebug() << "- Supported sample types : " << info.supportedSampleTypes();
+    qDebug() << "***********************************";
+}
 
 void checkAudioConverter() {
     if(QProcess::execute(Config::getInstance().checkMP3ConverterInstallationCommand()) != 0)
@@ -29,6 +45,7 @@ void init()
     qDebug("Initialization...");
     initDirectories();
     checkAudioConverter();
+    showAudioDeviceInfo();
 }
 
 
