0a3b597b0334630ecf0077d6394c6c9bdbc80c30
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-14 16:30:32 +0200

    Ajout d'un test

---
+++ tests/testJsonFile/tst_testjsonfile.cpp
@@ -0,0 +1,47 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class testJsonFile : public QObject
+{
+    Q_OBJECT
+
+public:
+    testJsonFile();
+    ~testJsonFile();
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void test_case1();
+
+};
+
+testJsonFile::testJsonFile()
+{
+
+}
+
+testJsonFile::~testJsonFile()
+{
+
+}
+
+void testJsonFile::initTestCase()
+{
+
+}
+
+void testJsonFile::cleanupTestCase()
+{
+
+}
+
+void testJsonFile::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(testJsonFile)
+
+#include "tst_testjsonfile.moc"
