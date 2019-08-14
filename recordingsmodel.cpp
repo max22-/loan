7d64e03b48f57827e350aca4088474a5b6cb3aab
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-14 15:47:46 +0200

    Ajout du titre des colonnes et recherche des fichiers JSON

--- recordingsmodel.cpp
+++ recordingsmodel.cpp
@@ -1,12 +1,15 @@
 #include "recordingsmodel.h"
+#include "config.h"
+#include <QDir>
 
 RecordingsModel::RecordingsModel(QObject *parent) : QAbstractTableModel (parent)
 {
-
+    inbox = Config::getInstance().inboxDirectory();
+    jsonFilesList = inbox.entryList(QStringList() << "*.json", QDir::Files, QDir::Name);
 }
 
 int RecordingsModel::rowCount(const QModelIndex &parent) const {
-    return 3;
+    return jsonFilesList.length();
 }
 
 int RecordingsModel::columnCount(const QModelIndex &parent) const {
@@ -15,7 +18,30 @@ int RecordingsModel::columnCount(const QModelIndex &parent) const {
 
 QVariant RecordingsModel::data(const QModelIndex &index, int role) const {
     if(role == Qt::DisplayRole) {
+        if(index.column() == 0)
+            return jsonFilesList.at(index.row());
         return QString::number(5);
+    }
+    return QVariant();
+}
+
+QVariant RecordingsModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if(role == Qt::DisplayRole) {
+        if(orientation == Qt::Horizontal) {
+            switch(section) {
+                case 0:
+                    return QString("Date");
+                case 1:
+                    return QString("Pseudonyme");
+                case 2:
+                    return QString("Âge");
+                case 3:
+                    return QString("Ville");
+                case 4:
+                    return QString("Evaluation");
+            }
+        }
     }
     return QVariant();
 }
--- recordingsmodel.h
+++ recordingsmodel.h
@@ -2,6 +2,7 @@
 #define RECORDINGSMODEL_H
 
 #include <QAbstractTableModel>
+#include <QDir>
 
 class RecordingsModel : public QAbstractTableModel
 {
@@ -11,6 +12,11 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
+private:
+    QStringList jsonFilesList;
+    QDir inbox;
 };
 
 #endif // RECORDINGSMODEL_H
