be6d4e7f0ff5b49f436296f15e2adab1880039ed
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-17 16:59:02 +0200

    Travail sur les tests

--- tests/testJsonFile/tst_testjsonfile.cpp
+++ tests/testJsonFile/tst_testjsonfile.cpp
@@ -1,5 +1,6 @@
 #include <QtTest>
 
+#include <QDir>
 #include "jsonfile.h"
 
 class testJsonFile : public QObject
@@ -13,13 +14,20 @@ public:
 private slots:
     void initTestCase();
     void cleanupTestCase();
-    void test_case1();
+    void init();
+    void cleanup();
+    void testSimpleLoad_data();
+    void testSimpleLoad();
+    void testSimpleSave();
+
+private:
+    QDir databaseDirectory;
 
 };
 
 testJsonFile::testJsonFile()
 {
-
+    databaseDirectory = QDir(QDir::current().absolutePath() + QDir::separator() + "database");
 }
 
 testJsonFile::~testJsonFile()
@@ -37,9 +45,65 @@ void testJsonFile::cleanupTestCase()
 
 }
 
-void testJsonFile::test_case1()
+void testJsonFile::init() {
+    if(databaseDirectory.mkpath(".") == false)
+        QFAIL("Could'nt create database directory.");
+}
+
+void testJsonFile::cleanup() {
+    if(!databaseDirectory.removeRecursively())
+        QFAIL("Couldn't remove database directory.");
+}
+
+void testJsonFile::testSimpleLoad_data() {
+    QTest::addColumn<QString>("json");
+    QTest::addColumn<QString>("nickname");
+    QTest::addColumn<int>("age");
+    QTest::addColumn<QString>("city");
+    QTest::addColumn<int>("evaluation");
+    QTest::addColumn<QString>("filename");
+    QTest::addColumn<int>("year");
+    QTest::addColumn<int>("month");
+    QTest::addColumn<int>("day");
+    QTest::addColumn<int>("hour");
+    QTest::addColumn<int>("minute");
+    QTest::addColumn<int>("second");
+
+    auto json1 = QString(
+        "{\"nickname\": \"Maxime\","
+        "\"age\": 33,"
+        "\"city\": \"Plérin\","
+        "\"evaluation\": 3,"
+        "\"filename\": \"2019-08-17 16:33:00.mp3\""
+        "\"timestamp\": \"2019-08-17 16:33:00\""
+        "}"
+    );
+
+    QTest::newRow("Maxime") << json1 << "Maxime" << 33 << "Plérin" << 3 << "2019-08-17 16:33:00.mp3" << 2019 << 8 << 17 << 16 << 33 << 0;
+
+    auto json2 = QString(
+        "{\"nickname\": \"J.J. Brun's\","
+        "\"age\": 20,"
+        "\"city\": \"Plérin\","
+        "\"evaluation\": 4,"
+        "\"filename\": \"2020-01-15 14:45:04.mp3\""
+        "\"timestamp\": \"2020-01-15 14:45:04\""
+        "}"
+    );
+
+    QTest::newRow("J.J. Brun's") << json2 << "J.J. Brun's" << 20 << "Plérin" << 4 << "2020-01-15 14:45:04.mp3" << 2020 << 1 << 15 << 14 << 45 << 4;
+
+}
+
+void testJsonFile::testSimpleLoad()
 {
+
     JsonFile jsonFile("/tmp/test.json");
+}
+
+void testJsonFile::testSimpleSave()
+{
+
 }
 
 QTEST_APPLESS_MAIN(testJsonFile)
