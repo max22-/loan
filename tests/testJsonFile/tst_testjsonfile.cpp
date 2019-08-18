90a1a2d93515a6e74f396ecee8887e9b35edfefb
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-18 16:57:51 +0200

    Ajout d'un nouvel exemple pour le test de chargement de fichier Json

--- tests/testJsonFile/tst_testjsonfile.cpp
+++ tests/testJsonFile/tst_testjsonfile.cpp
@@ -93,6 +93,18 @@ void testJsonFile::testSimpleLoad_data() {
 
     QTest::newRow("J.J. Brun's") << json2 << "J.J. Brun's" << 20 << "Plérin" << 4 << "2020-01-15 14:45:04.mp3" << 2020 << 1 << 15 << 14 << 45 << 4;
 
+    auto json3 = QString(
+        "{\"nickname\": \"Stéphane\","
+        "\"age\": 21,"
+        "\"city\": \"Plérin\","
+        "\"evaluation\": 5,"
+        "\"filename\": \"2021-03-27 09:32:27.mp3\""
+        "\"timestamp\": \"2021-03-27 09:32:27\""
+        "}"
+    );
+
+    QTest::newRow("Stéphane") << json3 << "Stéphane" << 21 << "Plérin" << 5 << "2021-03-27 09:32:27.mp3" << 2021 << 3 << 27 << 9 << 32 << 27;
+
 }
 
 void testJsonFile::testSimpleLoad()
