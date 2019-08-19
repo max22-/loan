7b6b0fd47e6454ad4cdf87eb46cbbcbf07869d9a
Maxime ANDRE <maxime.andre@gadz.org>
2019-08-19 21:49:18 +0200

    Travail sur les tests de la classe JsonFile

--- tests/testJsonFile/tst_testjsonfile.cpp
+++ tests/testJsonFile/tst_testjsonfile.cpp
@@ -56,12 +56,13 @@ void testJsonFile::cleanup() {
 }
 
 void testJsonFile::testSimpleLoad_data() {
-    QTest::addColumn<QString>("json");
+    QTest::addColumn<QString>("jsonFileName");
+    QTest::addColumn<QString>("jsonData");
     QTest::addColumn<QString>("nickname");
     QTest::addColumn<int>("age");
     QTest::addColumn<QString>("city");
     QTest::addColumn<int>("evaluation");
-    QTest::addColumn<QString>("filename");
+    QTest::addColumn<QString>("MP3FileName");
     QTest::addColumn<int>("year");
     QTest::addColumn<int>("month");
     QTest::addColumn<int>("day");
@@ -69,7 +70,8 @@ void testJsonFile::testSimpleLoad_data() {
     QTest::addColumn<int>("minute");
     QTest::addColumn<int>("second");
 
-    auto json1 = QString(
+    auto timeStamp1 = "2019-08-17 16:33:00";
+    auto jsonData1 = QString(
         "{\"nickname\": \"Maxime\","
         "\"age\": 33,"
         "\"city\": \"Plérin\","
@@ -79,9 +81,10 @@ void testJsonFile::testSimpleLoad_data() {
         "}"
     );
 
-    QTest::newRow("Maxime") << json1 << "Maxime" << 33 << "Plérin" << 3 << "2019-08-17 16:33:00.mp3" << 2019 << 8 << 17 << 16 << 33 << 0;
+    QTest::newRow("Maxime") << timeStamp1 + QString(".json") << jsonData1 << "Maxime" << 33 << "Plérin" << 3 << timeStamp1 + QString(".mp3") << 2019 << 8 << 17 << 16 << 33 << 0;
 
-    auto json2 = QString(
+    auto timeStamp2 = "2020-01-15 14:45:04";
+    auto jsonData2 = QString(
         "{\"nickname\": \"J.J. Brun's\","
         "\"age\": 20,"
         "\"city\": \"Plérin\","
@@ -91,9 +94,10 @@ void testJsonFile::testSimpleLoad_data() {
         "}"
     );
 
-    QTest::newRow("J.J. Brun's") << json2 << "J.J. Brun's" << 20 << "Plérin" << 4 << "2020-01-15 14:45:04.mp3" << 2020 << 1 << 15 << 14 << 45 << 4;
+    QTest::newRow("J.J. Brun's") << timeStamp2 + QString(".json") << jsonData2 << "J.J. Brun's" << 20 << "Plérin" << 4 << timeStamp2 + QString(".mp3") << 2020 << 1 << 15 << 14 << 45 << 4;
 
-    auto json3 = QString(
+    auto timeStamp3 = "2021-03-27 09:32:27";
+    auto jsonData3 = QString(
         "{\"nickname\": \"Stéphane\","
         "\"age\": 21,"
         "\"city\": \"Plérin\","
@@ -103,14 +107,46 @@ void testJsonFile::testSimpleLoad_data() {
         "}"
     );
 
-    QTest::newRow("Stéphane") << json3 << "Stéphane" << 21 << "Plérin" << 5 << "2021-03-27 09:32:27.mp3" << 2021 << 3 << 27 << 9 << 32 << 27;
+    QTest::newRow("Stéphane") << timeStamp3 + QString(".json") << jsonData3 << "Stéphane" << 21 << "Plérin" << 5 << timeStamp3 + QString(".mp3") << 2021 << 3 << 27 << 9 << 32 << 27;
 
 }
 
 void testJsonFile::testSimpleLoad()
 {
+    QFETCH(QString, jsonFileName);
+    QFETCH(QString, jsonData);
+    QFETCH(QString, nickname);
+    QFETCH(int, age);
+    QFETCH(QString, city);
+    QFETCH(int, evaluation);
+    QFETCH(QString, MP3FileName);
+    QFETCH(int, year);
+    QFETCH(int, month);
+    QFETCH(int, day);
+    QFETCH(int, hour);
+    QFETCH(int, minute);
+    QFETCH(int, second);
 
-    JsonFile jsonFile("/tmp/test.json");
+    QFile file(databaseDirectory.absoluteFilePath(jsonFileName));
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text))
+        QFAIL("Couldn't write to json file.");
+    QTextStream out(&file);
+    out << jsonData;
+    file.close();
+
+    JsonFile jsonFile(databaseDirectory.absoluteFilePath(jsonFileName));
+    jsonFile.load();
+    QCOMPARE(jsonFile.getNickname(), nickname);
+    QCOMPARE(jsonFile.getAge(), age);
+    QCOMPARE(jsonFile.getCity(), city);
+    QCOMPARE(jsonFile.getEvaluation(), evaluation);
+    QCOMPARE(jsonFile.getMP3FileName(), MP3FileName);
+    QCOMPARE(jsonFile.getTimeStamp().date().year(), year);
+    QCOMPARE(jsonFile.getTimeStamp().date().month(), month);
+    QCOMPARE(jsonFile.getTimeStamp().date().day(), day);
+    QCOMPARE(jsonFile.getTimeStamp().time().hour(), hour);
+    QCOMPARE(jsonFile.getTimeStamp().time().minute(), minute);
+    QCOMPARE(jsonFile.getTimeStamp().time().second(), second);
 }
 
 void testJsonFile::testSimpleSave()
